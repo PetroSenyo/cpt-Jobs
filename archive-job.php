390b2ded1a0176f45c289911dbc03f75cd90118c
petrosenyo <petrosenyo@gmail.com>
2024-08-14 13:21:18 +0300

    Add if for meta-box

--- archive-job.php
+++ archive-job.php
@@ -8,9 +8,23 @@
             <?php while ( have_posts() ) : the_post(); ?>
                 <li class="job-item">
                     <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
-                    <p><strong>Job Title:</strong> <?php echo esc_html(get_post_meta(get_the_ID(), 'job_title', true)); ?></p>
-                    <p><strong>Salary:</strong> <?php echo esc_html(get_post_meta(get_the_ID(), 'salary', true)); ?></p>
-                    <p><strong>Location:</strong> <?php echo esc_html(get_post_meta(get_the_ID(), 'location', true)); ?></p>
+                    <?php
+                    $job_title = get_post_meta(get_the_ID(), 'job_title', true);
+                    $salary = get_post_meta(get_the_ID(), 'salary', true);
+                    $location = get_post_meta(get_the_ID(), 'location', true);
+                    ?>
+
+                    <?php if (!empty($job_title)): ?>
+                        <p><strong>Job Title:</strong> <?php echo esc_html($job_title); ?></p>
+                    <?php endif; ?>
+
+                    <?php if (!empty($salary)): ?>
+                        <p><strong>Salary:</strong> <?php echo esc_html($salary); ?></p>
+                    <?php endif; ?>
+
+                    <?php if (!empty($location)): ?>
+                        <p><strong>Location:</strong> <?php echo esc_html($location); ?></p>
+                    <?php endif; ?>
                 </li>
             <?php endwhile; ?>
         </ul>
--- single-job.php
+++ single-job.php
@@ -3,9 +3,23 @@
 <div class="job-listings-container">
     <h1><?php the_title(); ?></h1>
 
-    <p><strong>Job Title:</strong> <?php echo get_post_meta(get_the_ID(), 'job_title', true); ?></p>
-    <p><strong>Salary:</strong> <?php echo get_post_meta(get_the_ID(), 'salary', true); ?></p>
-    <p><strong>Location:</strong> <?php echo get_post_meta(get_the_ID(), 'location', true); ?></p>
+    <?php
+    $job_title = get_post_meta(get_the_ID(), 'job_title', true);
+    $salary = get_post_meta(get_the_ID(), 'salary', true);
+    $location = get_post_meta(get_the_ID(), 'location', true);
+    ?>
+
+    <?php if (!empty($job_title)): ?>
+        <p><strong>Job Title:</strong> <?php echo esc_html($job_title); ?></p>
+    <?php endif; ?>
+
+    <?php if (!empty($salary)): ?>
+        <p><strong>Salary:</strong> <?php echo esc_html($salary); ?></p>
+    <?php endif; ?>
+
+    <?php if (!empty($location)): ?>
+        <p><strong>Location:</strong> <?php echo esc_html($location); ?></p>
+    <?php endif; ?>
 
     <div>
         <?php the_content(); ?>
